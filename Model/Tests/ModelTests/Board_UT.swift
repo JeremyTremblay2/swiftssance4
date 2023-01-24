71836095f4f25a019be71362e9cce2e77ac3dfff
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-25 00:10:29 +0100

    Add some unit tests to the Board class

--- Model/Tests/ModelTests/Board_UT.swift
+++ Model/Tests/ModelTests/Board_UT.swift
@@ -1,7 +1,25 @@
 import XCTest
+import Model
 
 final class Board_UT : XCTestCase {
     func testInit() throws {
-        
+        func expect(initBoardWithnumberOfRows rows: Int, numberOfColumns columns: Int, shouldBeNotNil nilValue: Bool) {
+            let board = Board(withNumberOfRows: rows, withNumberOfColumns: columns)
+            if  nilValue {
+                XCTAssertNil(board)
+                return
+            }
+            XCTAssertNotNil(board)
+            XCTAssertEqual(rows, board?.numberOfRows)
+            XCTAssertEqual(columns, board?.numberOfColumns)
+        }
+        expect(initBoardWithnumberOfRows: 6, numberOfColumns: 7, shouldBeNotNil: false)
+        expect(initBoardWithnumberOfRows: 2, numberOfColumns: 3, shouldBeNotNil: false)
+        expect(initBoardWithnumberOfRows: 1, numberOfColumns: 10, shouldBeNotNil: false)
+        expect(initBoardWithnumberOfRows: 10, numberOfColumns: 1, shouldBeNotNil: false)
+        expect(initBoardWithnumberOfRows: 0, numberOfColumns: 0, shouldBeNotNil: true)
+        expect(initBoardWithnumberOfRows: 6, numberOfColumns: -1, shouldBeNotNil: true)
+        expect(initBoardWithnumberOfRows: -1, numberOfColumns: 7, shouldBeNotNil: true)
+        expect(initBoardWithnumberOfRows: -1, numberOfColumns: -1, shouldBeNotNil: true)
     }
 }
