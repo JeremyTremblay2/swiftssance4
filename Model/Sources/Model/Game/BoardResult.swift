20ce72e50b7cd4229224e91bd559672326c05edc
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-27 00:43:20 +0100

    [#5] Transform the boardresults to be equatables

--- Model/Sources/Model/Game/BoardResult.swift
+++ Model/Sources/Model/Game/BoardResult.swift
@@ -1,13 +1,27 @@
-public enum BoardResult {
+public enum BoardResult : Equatable {
     case unknow
     case ok
     case failed(FailedReason)
+    
+    public static func == (lhs: BoardResult, rhs: BoardResult) -> Bool {
+       switch (lhs, rhs) {
+       case (.unknow, .unknow), (.ok, .ok):
+           return true
+       case let (.failed(lhsReason), .failed(rhsReason)):
+           return lhsReason == rhsReason
+       default:
+           return false
+       }
+   }
 }
 
-public enum FailedReason {
+public enum FailedReason : Equatable {
     case outOfBounds
     case columnFull
     case invalidPlayerId
-    case emptyColumn
-    case pieceAlreadyPresentAbove
+    case columnEmpty
+    
+    public static func == (lhs: FailedReason, rhs: FailedReason) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
 }
