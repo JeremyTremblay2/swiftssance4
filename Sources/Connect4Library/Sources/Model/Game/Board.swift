16f432b8435189d014599dfcea84ae9d346daffb
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-06 23:48:54 +0100

    [#10] Add documentation to the game package classes

--- Sources/Connect4Library/Sources/Model/Game/Board.swift
+++ Sources/Connect4Library/Sources/Model/Game/Board.swift
@@ -1,15 +1,29 @@
-/// 
-public struct Board : CustomStringConvertible, Hashable {
+/// The `Board` struct implements a Connect Four board game.
+/// The board consists of a 2D grid of cells, where each cell can contain an ID of a player (represented as 1 or 2) or `nil` if the cell is empty.
+/// The board implements the `CustomStringConvertible` and `Hashable` protocols.
+///
+/// # Notes
+///     - This is the main class of the project.
+///     - It contains multiple methods to collect and modify data.
+public struct Board: CustomStringConvertible, Hashable {
+    /// A default display string to be used when the cell is empty.
     private static let defaultDisplay = "-"
+    /// A mapping of player IDs to their correspoding display strings.
     private static let descriptionPlayerIDMapper: [Int?:String] = [nil:".", 1:"X", 2:"O"]
+    /// A list of available player IDs.
     private static let availableIds: [Int] = [1, 2]
+    /// The number of rows in the board.
     public let numberOfRows: Int
+    /// The number of columns in the board.
     public let numberOfColumns: Int
-    
+
+    /// The underlying grid data.
     private var grid: [[Int?]]
-    
+
+    /// A reversed version of the underlying grid, for display purposes.
     public var theGrid: [[Int?]] { grid.reversed() }
-    
+
+    /// A string representation of the board, to be used when the board is printed.
     public var description: String {
         let dashes = String(repeating: "-", count: numberOfColumns * 4 + 1)
         var string = String("\(dashes)\n")
@@ -23,7 +37,11 @@ public struct Board : CustomStringConvertible, Hashable {
         }
         return string
     }
-    
+
+    /// Initializes a new board with the specified number of rows and columns.
+    /// - Parameter numberOfRows: The number of rows in the board. Defaults to 6.
+    /// - Parameter numberOfColumns: The number of columns in the board. Defaults to 7.
+    /// - Returns: An optional `Board` object. If either the number of rows or columns is less than 1, `nil` is returned.
     public init?(withNumberOfRows numberOfRows: Int = 6, withNumberOfColumns numberOfColumns: Int = 7) {
         guard numberOfRows > 0 && numberOfColumns > 0 else {
             return nil
@@ -33,6 +51,9 @@ public struct Board : CustomStringConvertible, Hashable {
         grid = Array(repeating: Array(repeating: nil, count: numberOfColumns), count: numberOfRows)
     }
     
+    /// Initializes a new board with the specified grid.
+    /// - Parameter grid: The grid data to be used for the board.
+    /// - Returns: An optional `Board` object. If the grid data is empty or the number of columns in each row is incorrect, `nil` is returned.
     public init?(withGrid grid: [[Int?]]) {
         guard grid.count > 0 && grid[0].count > 0 else {
             return nil
@@ -50,45 +71,68 @@ public struct Board : CustomStringConvertible, Hashable {
             return nil
         }
     }
-    
+
+    /// Subscript to get the value of the grid at a specific row and column.
+    /// - Parameters:
+    ///   - row: The row index of the grid.
+    ///   - column: The column index of the grid.
+    /// - Returns: The value of the grid at the specified row and column or nil if it is empty.
     public subscript(row: Int, column: Int) -> Int? {
         get {
+            // Check if the bounds are valid.
             assert(boundsAreValid(atRow: row, andColumn: column), "Index out of bounds")
-            // Does not works in the subscript because the signature not precise that this scope can throw errors.
-            // throw Error()
             return grid[row][column]
         }
     }
     
+    /// Method to insert a piece into the grid at a specific column.
+    /// - Parameters:
+    ///   - id: The id of the player who wants to insert a piece.
+    ///   - column: The column where the player wants to insert a piece.
+    /// - Returns: A `BoardResult` indicating the result of the insertion.
     public mutating func insertPiece(from id: Int, atColumn column: Int) -> BoardResult {
+        // Check if the column is within the bounds of the grid.
         guard column >= 0 && column < numberOfColumns else {
             return .failed(.outOfBounds)
         }
+        // Find the first available row in the column to insert the piece.
         for r in 0..<numberOfRows {
             if grid[r][column] == nil {
                 return insertPiece(from: id, atRow: r, andColumn: column)
             }
         }
+        // Return .columnFull if the column is full and the piece cannot be inserted.
         return .failed(.columnFull)
     }
     
+    /// Method to remove a piece from a specific column.
+    /// - Parameters:
+    ///   - column: The column from which the piece should be removed.
+    /// - Returns: A `BoardResult` indicating the result of the removal.
     public mutating func removePiece(atColumn column: Int) -> BoardResult {
+        // Check if the column is within the bounds of the grid.
         guard (0..<numberOfColumns).contains(column) else {
             return .failed(.outOfBounds)
         }
+        // Return .columnEmpty if the column is already empty.
         if isEmpty(from: column) {
             return .failed(.columnEmpty)
         }
-        // Used to check if the piece is at the top of the grid. If it is not the case, cancel the action.
+        // Remove the piece from the top of the column.
         for r in (0..<numberOfRows).reversed() {
             if grid[r][column] != nil {
                 grid[r][column] = nil
                 return .ok
             }
         }
+        // Return .unknown if the removal was not successful.
         return .unknow
     }
     
+    /// Method to check if a specific column is full.
+    /// - Parameters:
+    ///   - column: The column to be checked.
+    /// - Returns: `true` if the column is full, `false` otherwise.
     public func isFull(atColumn column: Int) -> Bool {
         guard column >= 0 && column < numberOfColumns else {
             return false
@@ -101,6 +145,8 @@ public struct Board : CustomStringConvertible, Hashable {
         return true
     }
     
+    /// Determines if the board is full by checking if all columns are full.
+    /// - Returns: A Boolean indicating whether the board is full.
     public func isFull() -> Bool {
         for c in 0..<numberOfColumns {
             if !isFull(atColumn: c) {
@@ -110,6 +156,12 @@ public struct Board : CustomStringConvertible, Hashable {
         return true
     }
     
+    /// Attempts to insert a piece into the board at the specified row and column.
+    /// - Parameters:
+    ///   - id: The id of the player who is inserting the piece.
+    ///   - row: The row where the piece will be inserted.
+    ///   - column: The column where the piece will be inserted.
+    /// - Returns: A `BoardResult` indicating the result of the insertion.
     private mutating func insertPiece(from id: Int, atRow row: Int, andColumn column: Int) -> BoardResult {
         guard Board.availableIds.contains(id) else {
             return .failed(.invalidPlayerId)
@@ -121,6 +173,9 @@ public struct Board : CustomStringConvertible, Hashable {
         return .ok
     }
     
+    /// Determines if the grid is valid by checking if each cell contains a valid id.
+    /// - Parameter grid: The grid to be checked.
+    /// - Returns: A Boolean indicating whether the grid is valid.
     private func isGridValid(from grid: [[Int?]]) -> Bool {
         var valueWasFound: Bool
         for c in 0..<numberOfColumns {
@@ -138,6 +193,9 @@ public struct Board : CustomStringConvertible, Hashable {
         return true
     }
     
+    /// Determines if a column is empty by checking if any cell in the column contains a value.
+    /// - Parameter column: The column to be checked.
+    /// - Returns: A Boolean indicating whether the column is empty.
     private func isEmpty(from column: Int) -> Bool {
         for r in 0..<numberOfRows {
             if grid[r][column] != nil {
@@ -147,10 +205,17 @@ public struct Board : CustomStringConvertible, Hashable {
         return true
     }
     
+    /// Determines if the specified row and column are within the bounds of the board.
+    /// - Parameters:
+    ///   - row: The row to be checked.
+    ///   - column: The column to be checked.
+    /// - Returns: A Boolean indicating whether the specified row and column are within the bounds of the board.
     private func boundsAreValid(atRow row: Int, andColumn column: Int) -> Bool {
         return row >= 0 && row < numberOfRows && column >= 0 && column < numberOfColumns
     }
     
+    /// Overrides the equality operator to compare two Board instances.
+    /// Returns true if the grid arrays of both instances are equal, false otherwise.
     public static func == (lhs: Board, rhs: Board) -> Bool {
         for i in 0..<lhs.numberOfRows {
             for j in 0..<lhs.numberOfColumns {
@@ -162,6 +227,7 @@ public struct Board : CustomStringConvertible, Hashable {
         return true
     }
     
+    /// Implements the Hashable protocol by hashing the grid array.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(grid)
     }
--- Sources/Connect4Library/Sources/Model/Game/BoardResult.swift
+++ Sources/Connect4Library/Sources/Model/Game/BoardResult.swift
@@ -1,8 +1,14 @@
-public enum BoardResult : Equatable {
+/// The `BoardResult` enum represents the result of an operation performed on the `Board` class.
+///
+/// - unknow: The operation was not completed or its result is unknown.
+/// - ok: The operation was successful.
+/// - failed: The operation faild with a reason indicated by a `FailedReason` case.
+public enum BoardResult: Equatable {
     case unknow
     case ok
     case failed(FailedReason)
     
+    /// Returns a Boolean value indicating whether two `BoardResult` values are equal.
     public static func == (lhs: BoardResult, rhs: BoardResult) -> Bool {
        switch (lhs, rhs) {
        case (.unknow, .unknow), (.ok, .ok):
@@ -15,12 +21,19 @@ public enum BoardResult : Equatable {
    }
 }
 
-public enum FailedReason : Equatable {
+/// The `FailedReason` enum represents the reason why an operation performed on the `Board` class failed.
+///
+/// - outOfBounds: The operation was outside the bounds of the board.
+/// - columnFull: The operation was peformed on a full column.
+/// - invalidPlayerId: The operation was performed with an invalid player id.
+/// - columnEmpty: The operation was performed on an empty column.
+public enum FailedReason: Equatable {
     case outOfBounds
     case columnFull
     case invalidPlayerId
     case columnEmpty
     
+    /// Returns a Boolean value indicating whether two `FailedReason` values are equal.
     public static func == (lhs: FailedReason, rhs: FailedReason) -> Bool {
         return lhs.hashValue == rhs.hashValue
     }
