a7b0da6d6175686972cd929dfb8fe5cd3fd239bc
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-07 10:35:11 +0100

    [#10] Add more documentation to the utils package

--- Sources/Connect4Library/Sources/Model/Utils/Coordinate.swift
+++ Sources/Connect4Library/Sources/Model/Utils/Coordinate.swift
@@ -1,21 +1,42 @@
+/// Coordinate struct represents a coordinate on the game board.
+///
+/// It contains two properties x and y for the x and y position on the board, respectively.
+/// It also conforms to the Hashable and CustomStringConvertible protocols.
 public struct Coordinate : Hashable, CustomStringConvertible {
+    /// x-position of the coordinate on the game board.
     var x: Int
+    /// y-position of the coordinate on the game board.
     var y: Int
     
+    /// Computed property that returns the description of the `Coordinate` object as a string.
     public var description: String {
         "[\(x);\(y)]"
     }
     
-    init(atX x: Int, atY y: Int) {
+    /// Initializes a `Coordinate` object with the given x and y positions.
+    ///
+    /// - Parameters:
+    ///   - x: x-position of the coordinate on the game board.
+    ///   - y: y-position of the coordinate on the game board.
+    public init(atX x: Int, atY y: Int) {
         self.x = x
         self.y = y
     }
     
+    /// Function that enables the use of `Coordinate` as a key in a dictionary.
+    ///
+    /// - Parameter hasher: The `Hasher` instance to use for hashing.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(x)
         hasher.combine(y)
     }
     
+    /// Operator function that allows for comparison of two `Coordinate` objects.
+    ///
+    /// - Parameters:
+    ///   - lhs: Left-hand side `Coordinate` object to compare.
+    ///   - rhs: Right-hand side `Coordinate` object to compare.
+    /// - Returns: `true` if the two `Coordinate` objects have the same x and y positions, `false` otherwise.
     public static func ==(lhs: Coordinate, rhs: Coordinate) -> Bool {
         return lhs.x == rhs.x && lhs.y == rhs.y
     }
--- Sources/Connect4Library/Sources/Model/Utils/GameResult.swift
+++ Sources/Connect4Library/Sources/Model/Utils/GameResult.swift
@@ -1,9 +1,14 @@
-public enum GameResult {
+/// Represents the result of the game of Connect Four.
+public enum GameResult : Hashable {
+    /// Indicates that a player has won, taking the player's identifier and the coordinates of their winning sequence as arguments.
     case winner(Int, [Coordinate])
+    /// Indicates that the game has not yt finished.
     case notFinished
+    /// Indicates that the game ended in a draw.
     case equality
-    
-    static func ==(lhs: GameResult, rhs: GameResult) -> Bool {
+
+    /// Compares two `GameResult` values and returns `true` if they are equal, `false` otherwise.
+    public static func ==(lhs: GameResult, rhs: GameResult) -> Bool {
         switch (lhs, rhs) {
         case (.notFinished, .notFinished), (.equality, .equality):
             return true
